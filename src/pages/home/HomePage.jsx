00e077b60df8b62b063044ec6b22a80a288fc6d3
Anders Christensen <anders@medieskolerne.dk>
2024-08-29 22:22:00 +0200

    init

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,12 @@
+
+
+const HomePage = () => {
+
+    console.log('asd')
+    return (
+        <div className={"center-page"}>
+            Johnny
+        </div>
+    );
+};
+export default HomePage;
